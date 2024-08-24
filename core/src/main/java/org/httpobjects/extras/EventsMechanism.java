5d5cbe93e70d12c7987dca3a247c7c0f8b41d4d2
Stu Penrose <stu@penrose.us>
2024-08-23 17:24:34 -0700

    Got the 'extras' working with the new async api

--- core/src/main/java/org/httpobjects/extras/EventsMechanism.java
+++ core/src/main/java/org/httpobjects/extras/EventsMechanism.java
@@ -1,72 +1,81 @@
 package org.httpobjects.extras;
 
+import org.httpobjects.HttpObject;
+import org.httpobjects.Request;
+import org.httpobjects.Response;
+import org.httpobjects.SyncHttpObject;
+import org.httpobjects.eventual.Eventual;
+import org.httpobjects.util.HttpObjectUtil;
+import org.httpobjects.util.Method;
+
 public class EventsMechanism {
 
-//    public interface Events<Id> {
-//        Id onRequest(Request request);
-//        void onResponse(Id id, Response response);
-//        void onError(Throwable error);
-//    }
-//
-//    public static <Id> HttpObject onEvents(HttpObject thiss, Events<Id> events) {
-//        return eventsResource(thiss, events);
-//    }
-//
-//    private static <Id> HttpObject eventsResource(final HttpObject resource,
-//                                                  final Events<Id> events) {
-//        return new SyncBridge(resource.pattern()) {
-//
-//            private Response dec(Method method, Request req) {
-//                try {
-//                    Id id = events.onRequest(req);
-//                    EventualResult<Response> res = HttpObjectUtil.invokeMethod(resource, method, req);
-//                    events.onResponse(id, res);
-//                    return res;
-//                } catch (Throwable err) {
-//                    events.onError(err);
-//                    throw new RuntimeException(err);
-//                }
-//            }
-//
-//            @Override
-//            public Response deleteSync(Request req) {
-//                return dec(Method.DELETE, req);
-//            }
-//
-//            @Override
-//            public Response getSync(Request req) {
-//                return dec(Method.GET, req);
-//            }
-//
-//            @Override
-//            public Response headSync(Request req) {
-//                return dec(Method.HEAD, req);
-//            }
-//
-//            @Override
-//            public Response optionsSync(Request req) {
-//                return dec(Method.OPTIONS, req);
-//            }
-//
-//            @Override
-//            public Response postSync(Request req) {
-//                return dec(Method.POST, req);
-//            }
-//
-//            @Override
-//            public Response putSync(Request req) {
-//                return dec(Method.PUT, req);
-//            }
-//
-//            @Override
-//            public Response traceSync(Request req) {
-//                return dec(Method.TRACE, req);
-//            }
-//
-//            @Override
-//            public Response patchSync(Request req) {
-//                return dec(Method.PATCH, req);
-//            }
-//        };
-//    }
+    public interface Events<Id> {
+        Id onRequest(Request request);
+        void onResponse(Id id, Response response);
+        void onError(Throwable error);
+    }
+
+    public static <Id> HttpObject onEvents(HttpObject thiss, Events<Id> events) {
+        return eventsResource(thiss, events);
+    }
+
+    private static <Id> HttpObject eventsResource(final HttpObject resource,
+                                                  final Events<Id> events) {
+        return new SyncHttpObject(resource.pattern()) {
+
+            private Response dec(Method method, Request req) {
+                try {
+                    Id id = events.onRequest(req);
+                    Eventual<Response> e = HttpObjectUtil.invokeMethod(resource, method, req);
+                    Response res = e == null ? null : e.join();
+                    events.onResponse(id, res);
+                    return res;
+                } catch (Throwable err) {
+                    events.onError(err);
+                    throw new RuntimeException(err);
+                }
+            }
+
+            @Override
+            public Response deleteSync(Request req) {
+                return dec(Method.DELETE, req);
+            }
+
+            @Override
+            public Response getSync(Request req) {
+                return dec(Method.GET, req);
+            }
+
+            @Override
+            public Response headSync(Request req) {
+                return dec(Method.HEAD, req);
+            }
+
+            @Override
+            public Response optionsSync(Request req) {
+                return dec(Method.OPTIONS, req);
+            }
+
+            @Override
+            public Response postSync(Request req) {
+                return dec(Method.POST, req);
+            }
+
+            @Override
+            public Response putSync(Request req) {
+                return dec(Method.PUT, req);
+            }
+
+            @Override
+            public Response traceSync(Request req) {
+                return dec(Method.TRACE, req);
+            }
+
+            @Override
+            public Response patchSync(Request req) {
+                return dec(Method.PATCH, req);
+            }
+        };
+    }
 }
--- core/src/main/java/org/httpobjects/extras/MaskMechanism.java
+++ core/src/main/java/org/httpobjects/extras/MaskMechanism.java
@@ -1,103 +1,114 @@
 package org.httpobjects.extras;
 
+import org.httpobjects.*;
+import org.httpobjects.eventual.Eventual;
+import org.httpobjects.path.Path;
+import org.httpobjects.path.PathParamName;
+import org.httpobjects.path.PathPattern;
+import org.httpobjects.util.HttpObjectUtil;
+import org.httpobjects.util.Method;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MaskMechanism {
-//
-//    public static final HttpObject mask(HttpObject thiss, HttpObject that) {
-//        return maskResources(thiss, that, DSL.NOT_FOUND());
-//    }
-//
-//    public static final HttpObject mask(HttpObject thiss, HttpObject that, Representation notFound) {
-//        return maskResources(thiss, that, DSL.NOT_FOUND(notFound));
-//    }
-//
-//    private static PathPattern maskPatterns(final PathPattern left,
-//                                            final PathPattern right) {
-//        return new PathPattern() {
-//
-//            @Override
-//            public List<PathParamName> varNames() {
-//                List<PathParamName> result = new ArrayList<PathParamName>();
-//                result.addAll(left.varNames());
-//                result.addAll(right.varNames());
-//                return result;
-//            }
-//
-//            @Override
-//            public boolean matches(String path) {
-//                return left.matches(path) || right.matches(path);
-//            }
-//
-//            @Override
-//            public Path match(String path) {
-//                if (left.matches(path)) {
-//                    return left.match(path);
-//                } else {
-//                    return right.match(path);
-//                }
-//            }
-//
-//            @Override
-//            public String raw() {
-//                return left.raw() + ":" + right.raw();
-//            }
-//        };
-//    }
-//
-//    private static HttpObject maskResources(final HttpObject left,
-//                                            final HttpObject right,
-//                                            final Response notFound) {
-//        return new HttpObject(maskPatterns(left.pattern(), right.pattern())) {
-//
-//            private EventualResult<Response> match(Method method, Request req) {
-//                if (left.pattern().matches(req.path().toString())) {
-//                    return HttpObjectUtil.invokeMethod(left, method, req);
-//                } else if (right.pattern().matches(req.path().toString())) {
-//                    return HttpObjectUtil.invokeMethod(right, method, req);
-//                } else {
-//                    return new BasicEventualResult(notFound);
-//                }
-//            }
-//
-//            @Override
-//            public EventualResult<Response> delete(Request req) {
-//                return match(Method.DELETE, req);
-//            }
-//
-//            @Override
-//            public EventualResult<Response> get(Request req) {
-//                return match(Method.GET, req);
-//            }
-//
-//            @Override
-//            public EventualResult<Response> head(Request req) {
-//                return match(Method.HEAD, req);
-//            }
-//
-//            @Override
-//            public EventualResult<Response> options(Request req) {
-//                return match(Method.OPTIONS, req);
-//            }
-//
-//            @Override
-//            public EventualResult<Response> post(Request req) {
-//                return match(Method.POST, req);
-//            }
-//
-//            @Override
-//            public EventualResult<Response> put(Request req) {
-//                return match(Method.PUT, req);
-//            }
-//
-//            @Override
-//            public EventualResult<Response> trace(Request req) {
-//                return match(Method.TRACE, req);
-//            }
-//
-//            @Override
-//            public EventualResult<Response> patch(Request req) {
-//                return match(Method.PATCH, req);
-//            }
-//        };
-//    }
+
+    public static final HttpObject mask(HttpObject thiss, HttpObject that) {
+        return maskResources(thiss, that, DSL.NOT_FOUND());
+    }
+
+    public static final HttpObject mask(HttpObject thiss, HttpObject that, Representation notFound) {
+        return maskResources(thiss, that, DSL.NOT_FOUND(notFound));
+    }
+
+    private static PathPattern maskPatterns(final PathPattern left,
+                                            final PathPattern right) {
+        return new PathPattern() {
+
+            @Override
+            public List<PathParamName> varNames() {
+                List<PathParamName> result = new ArrayList<PathParamName>();
+                result.addAll(left.varNames());
+                result.addAll(right.varNames());
+                return result;
+            }
+
+            @Override
+            public boolean matches(String path) {
+                return left.matches(path) || right.matches(path);
+            }
+
+            @Override
+            public Path match(String path) {
+                if (left.matches(path)) {
+                    return left.match(path);
+                } else {
+                    return right.match(path);
+                }
+            }
+
+            @Override
+            public String raw() {
+                return left.raw() + ":" + right.raw();
+            }
+        };
+    }
+
+    private static HttpObject maskResources(final HttpObject left,
+                                            final HttpObject right,
+                                            final Response notFound) {
+        return new HttpObject(maskPatterns(left.pattern(), right.pattern())) {
+
+            private Eventual<Response> match(Method method, Request req) {
+                if (left.pattern().matches(req.path().toString())) {
+                    return HttpObjectUtil.invokeMethod(left, method, req);
+                } else if (right.pattern().matches(req.path().toString())) {
+                    return HttpObjectUtil.invokeMethod(right, method, req);
+                } else {
+                    return Eventual.resolved(notFound);
+                }
+            }
+
+            @Override
+            public Eventual<Response> delete(Request req) {
+                return match(Method.DELETE, req);
+            }
+
+            @Override
+            public Eventual<Response> get(Request req) {
+                return match(Method.GET, req);
+            }
+
+            @Override
+            public Eventual<Response> head(Request req) {
+                return match(Method.HEAD, req);
+            }
+
+            @Override
+            public Eventual<Response> options(Request req) {
+                return match(Method.OPTIONS, req);
+            }
+
+            @Override
+            public Eventual<Response> post(Request req) {
+                return match(Method.POST, req);
+            }
+
+            @Override
+            public Eventual<Response> put(Request req) {
+                return match(Method.PUT, req);
+            }
+
+            @Override
+            public Eventual<Response> trace(Request req) {
+                return match(Method.TRACE, req);
+            }
+
+            @Override
+            public Eventual<Response> patch(Request req) {
+                return match(Method.PATCH, req);
+            }
+        };
+    }
 
 }
